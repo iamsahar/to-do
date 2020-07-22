251a6828fd4f29aaf39a3bb4c588ac7367a31106
unknown <shr.farahzad@gmail.com>
2020-07-22 15:58:37 +0430

    add frontend

---
+++ frontend/build/precache-manifest.34054a29741fb1af0ccc0f15cffc07a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c69c2e4c3ae3bc29e255240c8902d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d985e03cbe48855b3418",
+    "url": "/static/css/main.d6bd9a29.chunk.css"
+  },
+  {
+    "revision": "e0f0dc00ae6ae6f602c3",
+    "url": "/static/js/2.e5f8debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5f8debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d985e03cbe48855b3418",
+    "url": "/static/js/main.6262319d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
